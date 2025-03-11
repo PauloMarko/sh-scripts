de32778514ad6e023a491713223dbfab1d0bec3b
Marko <marcospauloalve16@gmail.com>
2025-03-11 11:01:48 -0300

    Install and configure with this shell script (Quick)

--- crun.sh
+++ crun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/PauloMarko/sh-scripts/refs/heads/main/crun
 echo -e "\nif [ -f ~/.crun ]; then\n    . ~/.crun\nfi" >> ~/.bashrc
 source .bashrc
+wget https://raw.githubusercontent.com/PauloMarko/sh-scripts/refs/heads/main/crun
 mv crun .crun
 source .crun
 echo "CRun is ready to use!"
