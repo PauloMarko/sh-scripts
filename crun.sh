232f8725a344f136a399f7caa8a40e1cd3183672
Marko <marcospauloalve16@gmail.com>
2025-03-11 11:09:07 -0300

    Update crun.sh

--- crun.sh
+++ crun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo -e "\nif [ -f ~/.crun ]; then\n    . ~/.crun\nfi" >> ~/.bashrc
+if ! grep -q 'if \[ -f ~/.crun \]; then' ~/.bashrc; then
+    echo -e "\nif [ -f ~/.crun ]; then\n    . ~/.crun\nfi" >> ~/.bashrc
+fi
 source .bashrc
 wget https://raw.githubusercontent.com/PauloMarko/sh-scripts/refs/heads/main/crun
 mv crun .crun
